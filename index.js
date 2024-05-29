651baae4f0a0f0c0e333f5e926d58073e6442010
Edy Garcia <edygarcia271@gmail.com>
2024-05-29 17:45:57 -0600

    Calculadora_1

---
+++ index.js
@@ -0,0 +1,134 @@
+let uno = document.querySelector('#uno')
+let dos = document.querySelector('#dos')
+let tres = document.querySelector('#tres')
+let mas = document.querySelector('#mas')
+let cuatro = document.querySelector('#cuatro')
+let sinco = document.querySelector('#sinco')
+let seis = document.querySelector('#seis')
+let por = document.querySelector('#por')
+let siete = document.querySelector('#siete')
+let ocho = document.querySelector('#ocho')
+let nueve = document.querySelector('#nueve')
+let divicion = document.querySelector('#divicion')
+let cero = document.querySelector('#cero')
+let limpiar = document.querySelector('#limpiar')
+let menos = document.querySelector('#menos')
+let igual = document.querySelector('#igual')
+let barra = document.querySelector('#barra-number')
+
+let signo = ''
+let primerValor = 0
+let segundoValor = 0
+let numerosGuardados = 8
+let clave = 0
+
+
+
+
+uno.addEventListener('click', () => {
+    barra.value += parseInt(uno.value)
+})
+dos.addEventListener('click', () => {
+
+    barra.value += parseInt(dos.value)
+})
+tres.addEventListener('click', () => {
+    barra.value += tres.value
+})
+mas.addEventListener('click', () => {
+    primerValor = barra.value
+    console.log(primerValor)
+    signo = '+'
+    sumita = parseInt(primerValor) + (numerosGuardados)
+    console.log(sumita)
+    barra.value += sumita
+    console.log(typeof sudo);
+    barra.value = ""
+    
+    
+})
+cuatro.addEventListener('click', () => {
+    barra.value += cuatro.value
+})
+sinco.addEventListener('click', () => {
+    barra.value += sinco.value
+})
+seis.addEventListener('click', () => {
+    barra.value += seis.value
+})
+por.addEventListener('click', () => {
+    primerValor = barra.value
+    barra.value = ""
+    signo = '*'
+})
+siete.addEventListener('click', () => {
+    barra.value += siete.value
+})
+ocho.addEventListener('click', () => {
+    barra.value += ocho.value
+})
+nueve.addEventListener('click', () => {
+    barra.value += nueve.value
+})
+divicion.addEventListener('click', () => {
+    primerValor = barra.value
+    barra.value = ""
+    signo = '/'
+})
+cero.addEventListener('click', () => {
+    barra.value += cero.value
+})
+limpiar.addEventListener('click', () => {
+    barra.value += limpiar.value
+})
+menos.addEventListener('click', () => {
+    primerValor = barra.value
+    barra.value = ""
+    signo = '-'
+})
+igual.addEventListener('click', () => {
+    barra.value += igual.value
+})
+
+limpiar.addEventListener('click', () => {
+    barra.value = ""
+})
+
+igual.addEventListener('click', () => {
+
+    if (signo === '+') {
+        segundoValor = barra.value
+        barra.value = ""
+        console.log(segundoValor)
+        sumado = parseInt(primerValor) + parseInt(segundoValor)
+        console.log(sumado)
+        barra.value += sumado
+    }
+    else if (signo == '-') {
+        segundoValor = barra.value
+        barra.value = ""
+        console.log(segundoValor)
+        sumado = parseInt(primerValor) - parseInt(segundoValor)
+        console.log(sumado)
+        barra.value += sumado
+    }
+    else if (signo === '*') {
+        segundoValor = barra.value
+        barra.value = ""
+        console.log(segundoValor)
+        sumado = parseInt(primerValor) * parseInt(segundoValor)
+        console.log(sumado)
+        barra.value += sumado
+    }
+    else if (signo === '/') {
+        segundoValor = barra.value
+        barra.value = ""
+        console.log(segundoValor)
+        sumado = parseInt(primerValor) / parseInt(segundoValor)
+        console.log(sumado)
+        barra.value += sumado
+    }
+})
+
+
+
---
+++ readme.md
@@ -0,0 +1,5 @@
+# Calculadora Eventos
+
+Esto es una calculadora utilazando html css y Eventos de JavaScript
+
+![alt text](image.png)
